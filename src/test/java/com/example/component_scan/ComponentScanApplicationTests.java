9be8dde1784eefb679ceffc330094fc2afab8ce6
Minsung Son <sns2831@gmail.com>
2021-08-15 17:00:02 +0900

    chore: init project

---
+++ src/test/java/com/example/component_scan/ComponentScanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.component_scan;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ComponentScanApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
